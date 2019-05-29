8034c9097106cccdf99726bfedd82ce86a68ee9b
Jason Pacitti <jpacitti@gmail.com>
2019-05-29 17:25:59 -0500

    Added final run and updated tests script to use updated page method

--- tests/prod_smoke/nav_smoke_tests.py
+++ tests/prod_smoke/nav_smoke_tests.py
@@ -13,24 +13,25 @@ class NavSmokeTests(unittest.TestCase):
         self.ts = Status(self.driver)
 
     @pytest.mark.run(order=1)
-    def nav_tests(self):
+    def test_nav(self):
         self.tn.navigateAboutUs()
-        result1 = self.lp.verifyLoginTitle()
+        result1 = self.tn.verifyAboutUs()
         self.ts.mark(result1, 'Verify Title for About Us Page')
 
         self.tn.navigateDirectSales()
-        result2 = self.lp.verifyLoginTitle()
+        result2 = self.tn.verifyDirectSales()
         self.ts.mark(result2, 'Verify Title for Direct Sales page')
 
         self.tn.navigateProducts()
-        result3 = self.lp.verifyLoginTitle()
+        result3 = self.tn.verifyProducts()
         self.ts.mark(result3, 'Verify Title for Products page')
 
         self.tn.navigateContactUs()
-        result4 = self.lp.verifyLoginTitle()
+        result4 = self.tn.verifyContactUs()
         self.ts.mark(result4, 'Verify Title Contact Us page')
 
         self.tn.navigateRentalServices()
-        result5 = self.lp.verifyLoginTitle()
-        self.ts.mark(result5, 'Verify Title Rental Services')
+        result5 = self.tn.verifyRentalServices()
+        self.ts.markFinal('test_nav', result5, 'Verify Title Rental Services')
+
 
