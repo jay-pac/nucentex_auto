2c8c763ac846a9e6d9fc995daaa004d12267d7a1
Jason Pacitti <jpacitti@gmail.com>
2019-05-29 17:19:21 -0500

    Added validation tests for the Title page

--- pages/navigation/top_nav.py
+++ pages/navigation/top_nav.py
@@ -34,5 +34,35 @@ class TopNav(SeleniumDriver):
             self.elementClick(self._contact_us, locatorType='id')
 
     def verifyAboutUs(self):
+        if "NuCentury Textile Services - About Us" in self.getTitle():
+            return True
+        else:
+            return False
+
+    def verifyDirectSales(self):
+        if "NuCentury Textile Services - Direct Sales" in self.getTitle():
+            return True
+        else:
+            return False
+
+    def verifyRentalServices(self):
+        if "NuCentury Textile Services - Rental Services" in self.getTitle():
+            return True
+        else:
+            return False
+
+    def verifyProducts(self):
+        if "NuCentury Textile Services - Products" in self.getTitle():
+            return True
+        else:
+            return False
+
+    def verifyContactUs(self):
+        if "NuCentury Textile Services - Contact Us" in self.getTitle():
+            return True
+        else:
+            return False
+
+
         
 
--- tests/prod_smoke/nav_smoke_tests.py
+++ tests/prod_smoke/nav_smoke_tests.py
@@ -15,8 +15,22 @@ class NavSmokeTests(unittest.TestCase):
     @pytest.mark.run(order=1)
     def nav_tests(self):
         self.tn.navigateAboutUs()
+        result1 = self.lp.verifyLoginTitle()
+        self.ts.mark(result1, 'Verify Title for About Us Page')
+
         self.tn.navigateDirectSales()
+        result2 = self.lp.verifyLoginTitle()
+        self.ts.mark(result2, 'Verify Title for Direct Sales page')
+
         self.tn.navigateProducts()
+        result3 = self.lp.verifyLoginTitle()
+        self.ts.mark(result3, 'Verify Title for Products page')
+
         self.tn.navigateContactUs()
+        result4 = self.lp.verifyLoginTitle()
+        self.ts.mark(result4, 'Verify Title Contact Us page')
+
         self.tn.navigateRentalServices()
+        result5 = self.lp.verifyLoginTitle()
+        self.ts.mark(result5, 'Verify Title Rental Services')
 
